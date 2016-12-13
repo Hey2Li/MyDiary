627596812b94fea24360b40b1b6ca9566c5f079a
Lee2Go <hey2li@163.com>
2016-12-13 14:42:29 +0800

    项目初始化 已经资源添加

---
+++ MyDiary/MyDiary/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyDiary
+//
+//  Created by Tebuy on 16/12/13.
+//  Copyright © 2016年 Tebuy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
