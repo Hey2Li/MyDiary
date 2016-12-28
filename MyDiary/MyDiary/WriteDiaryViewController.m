c1ce2bb1d84417bebc58a2b68bb2d7608e0c316c
Lee2Go <hey2li@163.com>
2016-12-28 17:30:58 +0800

    no message

--- MyDiary/MyDiary/WriteDiaryViewController.m
+++ MyDiary/MyDiary/WriteDiaryViewController.m
@@ -21,7 +21,43 @@
 }
 - (void)initWithView{
     self.view.backgroundColor = [UIColor colorWithWhite:0 alpha:0.4];
+    self.writeView = [UIView new];
+    self.writeView.backgroundColor = [UIColor whiteColor];
+    self.writeView.layer.cornerRadius = 10.0f;
+    [self.view addSubview:self.writeView];
+    [self.writeView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.view.mas_right);
+        make.right.equalTo(self.view.mas_right);
+        make.top.equalTo(self.view.mas_top).offset(25);
+        make.bottom.equalTo(self.view.mas_bottom);
+    }];
     
+    UIButton *cancelBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [cancelBtn setImage:[UIImage imageNamed:@"diaryInfoBack"] forState:UIControlStateNormal];
+    [cancelBtn addTarget:self action:@selector(cancelBtn) forControlEvents:UIControlEventTouchUpInside];
+    [self.writeView addSubview:cancelBtn];
+    [cancelBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.writeView.mas_left).offset(10);
+        make.top.equalTo(self.writeView.mas_right).offset(10);
+        make.width.equalTo(@30);
+        make.height.equalTo(@30);
+    }];
+    
+    UILabel *yearLabel = [UILabel new];
+    yearLabel.text = @"2016 12.28 Tuesday";
+    yearLabel.font  =[UIFont systemFontOfSize:18];
+    yearLabel.textColor = kBlueColor;
+    [self.writeView addSubview:yearLabel];
+    [yearLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerY.equalTo(self.writeView.mas_centerY);
+        make.width.equalTo(@(kScreenWidth/2));
+        make.height.equalTo(@30);
+        make.top.equalTo(self.writeView.mas_top).offset(10);
+    }];
+    
+}
+- (void)cancelBtn{
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
